f76a02c6baf360435cf192ed2c0c829161689e9d
Robert Garza <robstudent1988@gmail.com>
2016-06-19 22:33:29 -0700

    Created MemeTBC and MemeCollectionVC, added XIB files in story board

---
+++ MemeMe.ObjC/MemeCollectionVC.h
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionVC.h
+//  MemeMe.ObjC
+//
+//  Created by Robert Garza on 6/19/16.
+//  Copyright © 2016 Robert Garza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemeCollectionVC : UICollectionViewController
+
+@end
